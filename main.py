500e1517b7b2c691b71319ceb2a96fc4e6aaed44
robyistrate <robyistrate@gmail.com>
2022-12-10 23:12:34 +0100

    test `main.py`

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ header = st.container()
 
 with header:
     st.title('Digital Environmental Footprint Calculator')
-    st.text('Quantify the environmental impacts of your Internet consumption habits \
-             to determine whether (and to what extent) Internet consumption could \
-             challenge a more environmentally sustainable lifestyle')
+    st.text('''
+            Quantify the environmental impacts of your Internet consumption habits
+            to determine whether and to what degree this could
+            challenge a more environmentally sustainable lifestyle
+            ''')
 
