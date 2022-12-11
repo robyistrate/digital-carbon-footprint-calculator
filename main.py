249cfb2d050bec90a1c18777e5ad2a2e12933b48
robyistrate <robyistrate@gmail.com>
2022-12-11 19:46:02 +0100

    test

--- main.py
+++ main.py
@@ -2,18 +2,28 @@ import streamlit as st
 import pandas as pd
 
 header = st.container()
-dataset = st.container()
+user_input = st.container()
 
 with header:
-    st.title('Digital Environmental Footprint Calculator')
+    st.title('Digital Carbon Footprint Calculator')
     st.text('''
-            Quantify the environmental impacts of your Internet consumption habits
-            to determine whether and to what degree this could challenge
-            a more environmentally sustainable lifestyle
+            Determine the climate impacts of your Internet consumption habits
             ''')
 
+list_of_countries = ['Global', 'Brazil', 'Canada', 'China', 'India', 'United States', 'Norway',
+                     'Switzerland', 'France', 'Denmark', 'Spain', 'Italy', 'Germany', 'Poland',
+                     'United Kingdom', 'South Africa', 'Australia', 'Russia']
 
-with dataset:
+with user_input:
+    st.header('Introduce your Internet consumption behavior')
+
+    sel_col, disp_col = st.columns(2)
+
+    # User location
+    user_loc = sel_col.selectbox('Select your country', options = sorted(list_of_countries), index='Global')
+    st.markdown('*If your country is not in the list, please select "Global"*')
+
+
     lca_data = pd.read_csv('data/lca_data_digital_content.csv')
     st.write(lca_data)
 
